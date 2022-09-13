21b335a0b0bc67ecdddc4c213b7d5ee058be5a8c
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-09-13 13:42:47 +0100

    Updated script.js
    Added class="container" for each canvas

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 //canvas1
 const canvas1 = document.getElementById('canvas1');
-const ctx = canvas1.getContext('2d');
+const ctx1 = canvas1.getContext('2d');
 const container1 = document.getElementById('container1');
 
 canvas1.width = container1.offsetWidth;
 canvas1.height = container1.offsetHeight;
 
-ctx.fillStyle = 'white';
-ctx.fillRect(10, 10, 50, 50);
+window.addEventListener('resize', function(){
+    canvas1.width = container1.offsetWidth;
+    canvas1.height = container1.offsetHeight;
+    ctx1.fillStyle = 'white';
+    ctx1.fillRect(10, 10, 50, 50);
+})
+
+
+
+
+//canvas2
+const canvas2 = document.getElementById('canvas2');
+const ctx2 = canvas2.getContext('2d');
+const container2 = document.getElementById('container2');
+
+canvas2.width = container2.offsetWidth;
+canvas2.height = container2.offsetHeight;
+
+window.addEventListener('resize', function(){
+    canvas2.width = container2.offsetWidth;
+    canvas2.height = container2.offsetHeight;
+    ctx2.fillStyle = 'white';
+    ctx2.fillRect(10, 10, 50, 50);
+})
