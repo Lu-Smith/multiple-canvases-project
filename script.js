c440139912fbfcdfbba258bf22df65ad4fdac1ba
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-09-13 14:16:25 +0100

    updated script.js

--- script.js
+++ script.js
@@ -9,13 +9,28 @@ canvas1.height = container1.offsetHeight;
 window.addEventListener('resize', function(){
     canvas1.width = container1.offsetWidth;
     canvas1.height = container1.offsetHeight;
-    ctx1.fillStyle = 'white';
-    ctx1.strokeStyle = 'grey';
-    ctx1.beginPath();
-    ctx1.arc(100, 100, 50, 0, Math.PI * 2);
-    ctx1.fill();
-
 })
+
+const mouse1 = {
+    x: null,
+    y: null,
+}
+
+canvas1.addEventListener('click', function(event) {
+    mouse1.x = event.x - canvas1.getBoundingClientRect().left;
+    mouse1.y = event.y - canvas1.getBoundingClientRect().top;
+    drawCircle1();
+})
+
+function drawCircle1() {
+    ctx1.fillStyle = '#4CDDE8';
+    ctx1.strokeStyle = '#56FFF8';
+    ctx1.lineWidth = 3;
+    ctx1.beginPath();
+    ctx1.arc(mouse1.x, mouse1.y, 20, 0, Math.PI * 2);
+    ctx1.fill();
+    ctx1.stroke();
+}
 
 
 
@@ -38,8 +53,8 @@ window.addEventListener('resize', function(){
     ctx2.lineTo(10, 80);
     ctx2.fill();
 
-    ctx2.fillStyle = 'red';
     ctx2.strokeStyle = 'red';
+    ctx2.lineWidth = 6;
     ctx2.beginPath();
     ctx2.moveTo(95, 95);
     ctx2.lineTo(95, 25);
