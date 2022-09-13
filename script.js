bf79ee27abbeb74f648ac8204ccba0f5f9194c06
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-09-13 14:51:25 +0100

    Updated script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const canvas1 = document.getElementById('canvas1');
 const ctx1 = canvas1.getContext('2d');
 const container1 = document.getElementById('container1');
+const particlesArray1 = [];
 
 canvas1.width = container1.offsetWidth;
 canvas1.height = container1.offsetHeight;
@@ -26,20 +27,50 @@ canvas1.addEventListener('mousemove', function(event) {
     mouse1.y = event.y - canvas1.getBoundingClientRect().top;
 })
 
-function drawCircle1() {
-    ctx1.fillStyle = '#4CDDE8';
-    ctx1.strokeStyle = '#56FFF8';
-    ctx1.lineWidth = 3;
-    ctx1.beginPath();
-    ctx1.arc(mouse1.x, mouse1.y, 20, 0, Math.PI * 2);
-    ctx1.fill();
-    ctx1.stroke();
+class Particle1 {
+    constructor(){
+        //this.x = mouse1.x;
+        //this.y = mouse1.y;
+        this.x = Math.random() * canvas1.width;
+        this.y = Math.random() * canvas1.height;
+        this.size = Math.random() * 15 + 1;
+        this.speedX = Math.random() * 3 - 1.5;
+        this.speedY = Math.random() * 3 - 1.5;
+    }
+    update(){
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
+    draw(){
+        ctx1.fillStyle = '#4CDDE8';
+        ctx1.strokeStyle = '#56FFF8';
+        ctx1.lineWidth = 3;
+        ctx1.beginPath();
+        ctx1.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx1.fill();
+        ctx1.stroke();
+    }
+}
+
+function init1(){
+    for ( let i = 0; i < 100; i++) {
+        particlesArray1.push(new Particle1());
+    }
+}
+
+init1();
+
+function handleParticle1() {
+    for ( let i = 0; i < particlesArray1.length; i++) {
+        particlesArray1[i].update();
+        particlesArray1[i].draw();
+    }
 }
 
 function animate1() {
     ctx1.clearRect(0, 0, canvas1.width, canvas1.height);
+    handleParticle1();
     requestAnimationFrame(animate1);
-    drawCircle1();
 }
 
 animate1();
